f80de2d21419e8fd8966e00a19837586d9ca1557
4cce8er <acceber5843@hotmail.com>
2022-11-23 18:36:06 +0100

    Update random number generator

--- cache_simulator/our_utils.h
+++ cache_simulator/our_utils.h
@@ -7,6 +7,6 @@ std::random_device ourRandDevice;
 std::mt19937 ourGenerator(ourRandDevice()); 
 
 int getRandInt(int min, int max) {
-    std::uniform_int_distribution<int> dist(min, max + 1); 
+    std::uniform_int_distribution<int> dist(min, max); 
     return dist(ourGenerator);
 }
