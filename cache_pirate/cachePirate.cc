285bf877cc732002f5a890b3d9cfac10fa511682
4cce8er <acceber5843@hotmail.com>
2022-11-30 14:32:33 +0100

    Update cache pirate

--- cache_pirate/cachePirate.cc
+++ cache_pirate/cachePirate.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "assert.h"
+#include <dlfcn.h>
 /** API: https://intel-pcm-api-documentation.github.io/classPCM.html */
 #include "cpucounters.h" // Intel PCM
 #include "mm_malloc.h"
@@ -37,16 +38,49 @@ public:
 
 int main(int argc, char* argv[]) 
 {
-    std::cout << "start" << std::endl;
+/*
+	void * handle = dlopen("libpcm.so", RTLD_LAZY);
+	if(!handle) {
+		printf("Abort: could not (dynamically) load shared library \n");
+		return -1;
+	}
+
+	pcm::PCM.pcm_c_build_core_event = (int (*)(uint8_t, const char *)) dlsym(handle, "pcm_c_build_core_event");
+	pcm::PCM.pcm_c_init = (int (*)()) dlsym(handle, "pcm_c_init");
+	pcm::PCM.pcm_c_start = (void (*)()) dlsym(handle, "pcm_c_start");
+	pcm::PCM.pcm_c_stop = (void (*)()) dlsym(handle, "pcm_c_stop");
+	pcm::PCM.pcm_c_get_cycles = (uint64_t (*)(uint32_t)) dlsym(handle, "pcm_c_get_cycles");
+	pcm::PCM.pcm_c_get_instr = (uint64_t (*)(uint32_t)) dlsym(handle, "pcm_c_get_instr");
+	pcm::PCM.pcm_c_get_core_event = (uint64_t (*)(uint32_t,uint32_t)) dlsym(handle, "pcm_c_get_core_event");
+
+
+	if(pcm::PCM.pcm_c_init == NULL || PCM.pcm_c_start == NULL || PCM.pcm_c_stop == NULL ||
+			PCM.pcm_c_get_cycles == NULL || PCM.pcm_c_get_instr == NULL ||
+			PCM.pcm_c_build_core_event == NULL || PCM.pcm_c_get_core_event == NULL)
+		return -1;
+
+    pcm::pcm_c_init();
+*/
+    pcm::PCM * m = pcm::PCM::getInstance();
+    m->program(pcm::PCM::DEFAULT_EVENTS, NULL);
+
+    if (m->program() != pcm::PCM::Success) return -1; 
+
     //const auto cpu_model = pcm::PCM::getInstance()->getCPUModel();
     pcm::SystemCounterState beforeState = pcm::getSystemCounterState();
 
     CachePirate cp(8 * 1024 * 1024, 16);
-    cp.pirate(8);
+    for (int i = 0; i < 10000; ++i) {
+        cp.pirate(8);
+    }
 
     pcm::SystemCounterState afterState = pcm::getSystemCounterState();
     uint64_t l3Misses = pcm::getL3CacheMisses(beforeState, afterState);
+    uint64_t l3Hits = pcm::getL3CacheHits(beforeState, afterState);
+    uint64_t cycles = pcm::getCycles(beforeState, afterState);
 
     std::cout << l3Misses << std::endl;
+    std::cout << l3Hits << std::endl;
+    std::cout << cycles << std::endl;
     return 0;
 }
--- cache_pirate/source.sh
+++ cache_pirate/source.sh
@@ -1,4 +1,4 @@
-MY_PCM_PATH=~/git-repos/pcm     # Change this accordingly
+MY_PCM_PATH=/home/acceber/git-repos/pcm     # Change this accordingly
 
 export PCM_PATH=$MY_PCM_PATH
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PCM_PATH}/build/lib
